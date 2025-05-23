79b360c33733d81da6f0657b04a787bc5409bbe4
vleronemehmeti <vm64885@ubt-uni.net>
2025-05-24 00:25:06 +0200

    Product Loading

---
+++ e-commerce/components/ProductCard.tsx
@@ -0,0 +1,10 @@
+import { Product } from "@/sanity.types";
+import React from "react";
+
+
+const ProductCard = ({ product }: { product: Product }) => {
+  return <div>ProductCard</div>;
+
+};
+
+export default ProductCard;
--- e-commerce/components/ui/ProductGrid.tsx
+++ e-commerce/components/ui/ProductGrid.tsx
@@ -1,9 +1,13 @@
 "use client";
 import React, { useEffect, useState } from 'react';
 import HomeTabbar from './HomeTabbar';
+import ProductCard from "@/components/ProductCard";
 import { productType } from '@/constants';
 import { client } from '@/sanity/lib/client';
 import { Product } from '@/sanity.types';
+import NoProductsAvailable from '../NoProductsAvailable';
+import { motion, AnimatePresence } from "motion/react";
+import { Loader2 } from "lucide-react";
 
 const ProductGrid = () => {
   const [selectedTab, setSelectedTab] = useState(productType[0]?.title || "");
@@ -34,13 +38,29 @@ const ProductGrid = () => {
     <div className='mt-10 flex flex-col items-center'>
       <HomeTabbar selectedTab={selectedTab} onTabSelect={setSelectedTab} />
       {loading ? (
-        <div>
-          <span>Product is loading...</span>
+        <div className="flex flex-col items-center justify-center py-10 min-h-80 space-y-4 text-center bg-gray-100 rounded-lg w-full mt-10">
+          <div className="flex items-center space-x-2 text-blue-600">
+            <Loader2 className="animate-spin" />
+            <span className="text-lg font-semibold">Product is loading...</span>
+          </div>
+       
         </div>
       ) : (
-        products?.map((item: Product) => (
-          <p key={item?._id}>{item?.name}</p>
-        ))
+        <>
+          {products?.length ? (
+            <div className="grid grid-cols-2 md:grid-cols-3 
+            lg:grid-cols-4 gap-8 mt-10 w-full">
+              {products.map((product: Product) => (
+              <div key={product?._id}>
+                <ProductCard product={product} />
+              </div>
+            ))}
+            </div>
+          
+          ) : (
+            <NoProductsAvailable selectedTab={selectedTab} />
+          )}
+        </>
       )}
     </div>
   );
